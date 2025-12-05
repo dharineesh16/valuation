c13799cf1c65287446cf4ff8b5af6126a31d2ebe
dharineesh <dharaneeshyuvaraj591@gmail.com>
2025-12-05 11:59:29 +0530

    this is a doctype

---
+++ valuation_app/valuation_app/doctype/valuation/test_valuation.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, dharineesh and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestValuation(FrappeTestCase):
+	pass
