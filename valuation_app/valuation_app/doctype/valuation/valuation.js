069a7494136033093099381d7d27c533fbba4d1d
dharineesh <dharaneeshyuvaraj591@gmail.com>
2026-02-03 10:13:13 +0530

    this is inital message

--- valuation_app/valuation_app/doctype/valuation/valuation.js
+++ valuation_app/valuation_app/doctype/valuation/valuation.js
@@ -1,8 +1,205 @@
 // Copyright (c) 2025, dharineesh and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Valuation", {
-// 	refresh(frm) {
+frappe.ui.form.on('Valuation', {
 
-// 	},
-// });
+    refresh(frm) {
+        recalculate_all(frm);
+    },
+
+    // -----------------------------
+    // BASE FIELD SYNC
+    // -----------------------------
+    yt: frm => sync_and_recalc(frm, 'jn', 'yt'),
+    priya_p: frm => sync_and_recalc(frm, 'vidu_r', 'priya_p'),
+    cf_c: frm => sync_and_recalc(frm, 'fr', 'cf_c'),
+    promo_p: frm => sync_and_recalc(frm, 'youth_y', 'promo_p'),
+
+    // -----------------------------
+    // GUIDELINE / MARKET
+    // -----------------------------
+    as_per_guideline_value: recalculate_all,
+    rate_for_sft: recalculate_all,
+    as_per_market_rate: recalculate_all,
+    mr_rate_for_sft: recalculate_all,
+
+    // -----------------------------
+    // APPROVED / ACTUAL
+    // -----------------------------
+    ae_per_approved_plan: recalculate_all,
+    rp_as_per_approved_plan: recalculate_all,
+    as_per_actual_2: recalculate_all,
+    vp_as_per_actual: recalculate_all,
+    rate_2: recalculate_all,
+    iu_i: recalculate_all,
+
+    // -----------------------------
+    // AMENITIES
+    // -----------------------------
+    water_sump: recalculate_all,
+    septic_tank: recalculate_all,
+    bore: recalculate_all,
+    head_room: recalculate_all,
+    lo_l: recalculate_all,
+    ty_t: recalculate_all,
+    tl_t: recalculate_all,
+    mn_m: recalculate_all
+});
+
+/* =====================================================
+   HELPERS
+===================================================== */
+
+const n = v => flt(v || 0);
+
+function sync_and_recalc(frm, target, source) {
+    frm.set_value(target, n(frm.doc[source]));
+    recalculate_all(frm);
+}
+
+/* =====================================================
+   MASTER PIPELINE (ONE ENTRY POINT)
+===================================================== */
+
+function recalculate_all(frm) {
+    calculate_areas(frm);
+    calculate_guideline_market(frm);
+    calculate_approved_actual(frm);
+    calculate_amenities_and_totals(frm);
+    calculate_summary(frm);
+}
+
+/* =====================================================
+   AREA CALCULATIONS
+===================================================== */
+
+function calculate_areas(frm) {
+
+    const yt = n(frm.doc.yt);
+    const priya = n(frm.doc.priya_p);
+    const cf = n(frm.doc.cf_c);
+    const promo = n(frm.doc.promo_p);
+
+    frm.set_value('freedom', yt + priya);
+
+    const hg = yt * 0.9;
+    const remo = priya * 0.9;
+    frm.set_value('hg', hg);
+    frm.set_value('remo_r', remo);
+    frm.set_value('total_in_sft', hg + remo);
+
+    const dc = cf * 0.9;
+    const board = promo * 0.9;
+    frm.set_value('dc', dc);
+    frm.set_value('board_er', board);
+    frm.set_value('cp_total_in_sft', dc + board);
+
+    frm.set_value('to_total_in_sft', n(frm.doc.jn) + n(frm.doc.vidu_r));
+    frm.set_value('fortness', n(frm.doc.fr) + n(frm.doc.youth_y));
+    frm.set_value('sa_total_in_sft', n(frm.doc.fr) + n(frm.doc.youth_y));
+}
+
+/* =====================================================
+   GUIDELINE & MARKET
+===================================================== */
+
+function calculate_guideline_market(frm) {
+
+    frm.set_value(
+        'tron',
+        n(frm.doc.as_per_guideline_value) * n(frm.doc.rate_for_sft)
+    );
+
+    frm.set_value(
+        'dr_d',
+        n(frm.doc.as_per_market_rate) * n(frm.doc.mr_rate_for_sft)
+    );
+}
+
+/* =====================================================
+   APPROVED & ACTUAL
+===================================================== */
+
+function calculate_approved_actual(frm) {
+
+    const hit =
+        n(frm.doc.ae_per_approved_plan) * n(frm.doc.rate_2);
+
+    const float_htmj =
+        n(frm.doc.rp_as_per_approved_plan) * n(frm.doc.iu_i);
+
+    const jack =
+        n(frm.doc.vp_as_per_actual) * n(frm.doc.rate_2);
+
+    const ip_i =
+        n(frm.doc.as_per_actual_2) * n(frm.doc.iu_i);
+
+    frm.set_value('hit', hit);
+    frm.set_value('float_htmj', float_htmj);
+    frm.set_value('jack', jack);
+    frm.set_value('ip_i', ip_i);
+
+    frm.set_value('ravi', hit + float_htmj);
+    frm.set_value('ipl_i', jack + ip_i);
+}
+
+/* =====================================================
+   AMENITIES & TOTALS
+===================================================== */
+
+function calculate_amenities_and_totals(frm) {
+
+    const amenities =
+        n(frm.doc.water_sump) +
+        n(frm.doc.septic_tank) +
+        n(frm.doc.bore) +
+        n(frm.doc.head_room);
+
+    const amenities_alt =
+        n(frm.doc.lo_l) +
+        n(frm.doc.ty_t) +
+        n(frm.doc.tl_t) +
+        n(frm.doc.mn_m);
+
+    frm.set_value('amentities', amenities);
+    frm.set_value('jkl_j', amenities_alt);
+
+    const building = n(frm.doc.ravi) + amenities;
+    const building_alt = n(frm.doc.ipl_i) + amenities_alt;
+
+    frm.set_value('total_buliding_value_amentities', building);
+    frm.set_value('jk_total_buliding_amentities', building_alt);
+
+    frm.set_value('total_guide_line', n(frm.doc.tron) + building);
+    frm.set_value('iopl_total_guide_line', n(frm.doc.tron) + building_alt);
+
+    frm.set_value('total_fair_market_value', n(frm.doc.dr_d) + building);
+    frm.set_value('khj_total_fair_market', n(frm.doc.dr_d) + building_alt);
+
+    frm.set_value('realizable_value', n(frm.doc.total_fair_market_value) * 0.9);
+    frm.set_value('piol_realizable_value', n(frm.doc.khj_total_fair_market) * 0.9);
+
+    frm.set_value('forced_distressed_value', n(frm.doc.total_fair_market_value) * 0.8);
+    frm.set_value('yuo_forced_distressed_value', n(frm.doc.khj_total_fair_market) * 0.8);
+}
+
+/* =====================================================
+   SUMMARY (READ-ONLY, FINAL OUTPUT)
+===================================================== */
+
+function calculate_summary(frm) {
+
+    frm.set_value('uoyt_glr_value', n(frm.doc.total_guide_line));
+    frm.set_value('glr_value', n(frm.doc.iopl_total_guide_line));
+
+    frm.set_value('iuyt_market_value', n(frm.doc.total_fair_market_value));
+    frm.set_value('fair_market', n(frm.doc.khj_total_fair_market));
+
+    frm.set_value('uyt_realisable_value', n(frm.doc.realizable_value));
+    frm.set_value('realisable_value', n(frm.doc.piol_realizable_value));
+
+    frm.set_value('iutyre_distress_sale_value', n(frm.doc.forced_distressed_value));
+    frm.set_value('ip_distress_sale_value', n(frm.doc.yuo_forced_distressed_value));
+    frm.set_value( 'net_cost_after_depreciation', n(frm.doc.ravi) );
+    frm.set_value( 'net_cost_after_depreciation_1', n(frm.doc.ipl_i) );
+}
--- valuation_app/valuation_app/doctype/valuation/valuation.py
+++ valuation_app/valuation_app/doctype/valuation/valuation.py
@@ -1,9 +1,135 @@
 # Copyright (c) 2025, dharineesh and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import flt
 
 
 class Valuation(Document):
-	pass
+
+    def before_save(self):
+        """Run all backend calculations safely"""
+        self.sync_base_fields()
+        self.calculate_areas()
+        self.calculate_guideline_market()
+        self.calculate_approved_actual()
+        self.amenities_calculations()
+        self.summary_of_calculations()
+
+    # -------------------------------------------------
+    # FIELD SYNC
+    # -------------------------------------------------
+
+    def sync_base_fields(self):
+        self.jn = flt(self.yt)
+        self.vidu_r = flt(self.priya_p)
+        self.fr = flt(self.cf_c)
+        self.youth_y = flt(self.promo_p)
+
+    # -------------------------------------------------
+    # AREA CALCULATIONS
+    # -------------------------------------------------
+
+    def calculate_areas(self):
+        yt = flt(self.yt)
+        priya_p = flt(self.priya_p)
+        cf_c = flt(self.cf_c)
+        promo_p = flt(self.promo_p)
+
+        # Plinth
+        self.freedom = yt + priya_p
+
+        # Carpet
+        self.hg = yt * 0.9
+        self.remo_r = priya_p * 0.9
+        self.total_in_sft = self.hg + self.remo_r
+
+        # CP
+        self.dc = cf_c * 0.9
+        self.board_er = promo_p * 0.9
+        self.cp_total_in_sft = self.dc + self.board_er
+
+        # Saleable
+        self.to_total_in_sft = flt(self.jn) + flt(self.vidu_r)
+        self.fortness = flt(self.fr) + flt(self.youth_y)
+        self.sa_total_in_sft = self.fortness
+
+    # -------------------------------------------------
+    # GUIDELINE & MARKET
+    # -------------------------------------------------
+
+    def calculate_guideline_market(self):
+        self.tron = flt(self.as_per_guideline_value) * flt(self.rate_for_sft)
+        self.dr_d = flt(self.as_per_market_rate) * flt(self.mr_rate_for_sft)
+
+    # -------------------------------------------------
+    # APPROVED VS ACTUAL
+    # -------------------------------------------------
+
+    def calculate_approved_actual(self):
+        self.hit = flt(self.ae_per_approved_plan) * flt(self.rate_2)
+        self.float_htmj = flt(self.rp_as_per_approved_plan) * flt(self.iu_i)
+
+        self.jack = flt(self.vp_as_per_actual) * flt(self.rate_2)
+        self.ip_i = flt(self.as_per_actual_2) * flt(self.iu_i)
+
+        self.ravi = self.hit + self.float_htmj
+        self.ipl_i = self.jack + self.ip_i
+
+    # -------------------------------------------------
+    # AMENITIES
+    # -------------------------------------------------
+
+    def amenities_calculations(self):
+        self.amentities = (
+            flt(self.water_sump)
+            + flt(self.septic_tank)
+            + flt(self.bore)
+            + flt(self.head_room)
+        )
+
+        self.jkl_j = (
+            flt(self.lo_l)
+            + flt(self.ty_t)
+            + flt(self.tl_t)
+            + flt(self.mn_m)
+        )
+
+        self.total_buliding_value_amentities = flt(self.ravi) + self.amentities
+        self.jk_total_buliding_amentities = flt(self.ipl_i) + self.jkl_j
+
+        self.total_guide_line = flt(self.tron) + self.total_buliding_value_amentities
+        self.iopl_total_guide_line = flt(self.tron) + self.jk_total_buliding_amentities
+
+        self.total_fair_market_value = flt(self.dr_d) + self.total_buliding_value_amentities
+        self.khj_total_fair_market = flt(self.dr_d) + self.jk_total_buliding_amentities
+
+        self.realizable_value = self.total_fair_market_value * 0.9
+        self.piol_realizable_value = self.khj_total_fair_market * 0.9
+
+        self.forced_distressed_value = self.total_fair_market_value * 0.8
+        self.yuo_forced_distressed_value = self.khj_total_fair_market * 0.8
+
+    # -------------------------------------------------
+    # SUMMARY (FIELD ASSIGNMENTS ONLY)
+    # -------------------------------------------------
+
+    def summary_of_calculations(self):
+        """
+        Store final summary values into DocFields
+        (Do NOT return dict – Frappe ignores it)
+        """
+        self.glr_value = flt(self.total_guide_line)
+        self.as_glr_value = flt(self.iopl_total_guide_line)
+
+        self.iuyt_market_value = flt(self.total_fair_market_value)
+        self.per_fair_market = flt(self.khj_total_fair_market)
+
+        self.uyt_realisable_value = flt(self.realizable_value)
+        self.act_realisable_value = flt(self.piol_realizable_value)
+
+        self.iutyre_distress_sale_value = flt(self.forced_distressed_value)
+        self.ip_distress_sale_value = flt(self.yuo_forced_distressed_value)
+        self.ravi = self.net_cost_after_depreciation
+        self.ipl_i = self.net_cost_after_depreciation_1
